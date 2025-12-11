fe0f59a0fd58c869a40689280fbdccc01b5365f9
Shivani Verma <vshivani911@gmail.com>
2025-12-11 13:17:19 +0530

    Initial Xcode project: SwiftUI app setup

---
+++ miniRecipe/miniRecipe/miniRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniRecipeApp.swift
+//  miniRecipe
+//
+//  Created by Shivani Verma on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct miniRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
